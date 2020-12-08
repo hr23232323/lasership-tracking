a5e0a8f3d90c9fa58c4ce901dda49a3215c0642c
Harsh Rana <hr23232323@gmail.com>
2020-12-08 09:55:58 -0500

    updated to be more error resistant

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ import json, time, os
 
 def main():
     # Replace with your own tracking number
-    trackingNum = "1LS7306076435114-1"
+    trackingNum = "1LS7306077308792-1"
 
     # Loop runs while item is not delivered
     while True:
-        # Use lasership API to get info
-        res = req.post(f"http://www.lasership.com/track/{trackingNum}/json")
-        resJson = json.loads(res.text)
+        try:
+            # Use lasership API to get info
+            res = req.post(f"http://www.lasership.com/track/{trackingNum}/json")
+            resJson = json.loads(res.text)
 
-        # Status of most recent event in API res
-        curStatus = resJson["Events"][0]["EventLabel"]
+            # Status of most recent event in API res
+            curStatus = resJson["Events"][0]["EventLabel"]
 
-        # Timestamp for tracking
-        t = time.localtime()
-        curTime = time.strftime("%H:%M:%S", t)
+            # Timestamp for tracking
+            t = time.localtime()
+            curTime = time.strftime("%H:%M:%S", t)
 
-        # Print TS and status so user is aware of running program
-        print(curTime + " : " + curStatus)
-
+            # Print TS and status so user is aware of running program
+            print(curTime + " : " + curStatus)
+        except:
+            print("Error with API, will sleep and proceed")
+            
         # If out for delivery, sleep for 30s, otherwise break
-        if(curStatus != "Out for Delivery"):
+        if(curStatus == "Delivered"):
             break
         else:
             time.sleep(30)
